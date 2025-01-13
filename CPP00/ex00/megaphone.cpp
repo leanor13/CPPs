0d7bc39111ddfad587449b2e4649ae2b86482a5c
Yulia Ioffe <yumioffe@gmail.com>
2025-01-13 20:01:20 +0000

    remove brackets

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -4,20 +4,16 @@ int	main(int ac, char **av)
 {
 	(void) av;
 	if (ac == 1)
-	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl; 
-	}
 	else
 	{
 		for (int i = 1; i < ac; i ++)
-		{
 			for (int j = 0; av[i][j] != '\0'; j ++)
 			{
 				std::cout << (char)toupper(av[i][j]);
 				if (av[i][j + 1] == '\0' && i != ac - 1)
     				std::cout << ' ';
 			}
-		}
 		std::cout << std::endl;
 	}
 	return (0);
