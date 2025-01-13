6dfd8f9198f1803ea3acc5a288b2a58b7952d2e3
Yulia Ioffe <yumioffe@gmail.com>
2025-01-13 17:57:59 +0000

    init cpp00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	(void) av;
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl; 
+	}
+	else
+	{
+
+	}
+	return (0);
+
+
+}
