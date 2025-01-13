1f123533caadfaa1718c95e1b30f8da8e0c44322
Yulia Ioffe <yumioffe@gmail.com>
2025-01-13 19:58:59 +0000

    megaphone

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -9,9 +9,16 @@ int	main(int ac, char **av)
 	}
 	else
 	{
-
+		for (int i = 1; i < ac; i ++)
+		{
+			for (int j = 0; av[i][j] != '\0'; j ++)
+			{
+				std::cout << (char)toupper(av[i][j]);
+				if (av[i][j + 1] == '\0' && i != ac - 1)
+    				std::cout << ' ';
+			}
+		}
+		std::cout << std::endl;
 	}
 	return (0);
-
-
 }
